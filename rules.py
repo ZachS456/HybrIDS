923134c7db426289046b468619ef8ad9f022f61b
Ben <BenProjex@users.noreply.github.com>
2016-11-22 19:33:06 -0600

    Finished rules parser

--- rules.py
+++ rules.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 
 import sys
-import numpy
+import re
 
 def getRules(fileName):
-   
+
    fin = open(fileName, 'r')
-   lines = fin.read()
-
-   checkRules(lines)
-
-   return lines
+   lines = fin.readlines()
+   rulesD = checkRules(lines)
+   print str(rulesD)
+   return rulesD
 
 def checkRules(lines):
+   rulesList = {'proto': [],
+                'srcip': [],
+                'srcport': [],
+                'dstip': [],
+                'dstport': [],
+                'content': [],
+                'msg': []
+   }
+
    for line in lines:
+      if '#' in line:
+         continue
       tokens = line.split(' ', 6)
       if len(tokens) < 7:
          sys.stderr.write("Rule syntax error!");
@@ -21,5 +31,12 @@ def checkRules(lines):
       elif not isinstance(tokens[6], str):
          sys.stderr.write("Incorrect message!");
          exit(-1)
-
-
+      else:
+         rulesList['proto'].append(tokens[0])
+         rulesList['srcip'].append(tokens[1])
+         rulesList['srcport'].append(tokens[2])
+         rulesList['dstip'].append(tokens[3])
+         rulesList['dstport'].append(tokens[4])
+         rulesList['content'].append(tokens[5])
+         rulesList['msg'].append( tokens[6])
+   return rulesList
