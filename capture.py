bab791d5add58032c8a59876ea0d4131557d07f1
Ben <BenProjex@users.noreply.github.com>
2016-11-22 20:52:36 -0600

    current capture

--- capture.py
+++ capture.py
@@ -7,31 +7,39 @@ def cap():
    sniff(iface = 'eth0', prn=processPacket, store=0)
 
 def processPacket(pkt):
-   processHeader(pkt)
-   processData(pkt)
+   rules = pullRules()
+   pktInfo = processHeader(pkt, rules)
+   pktInfo['content'] = str(pkt).encode("HEX")
+   #processData(pktInfo)
 
 def processHeader(pkt):
    if IP in pkt:
       ipSrc = pkt[IP].src
       ipDst = pkt[IP].dst
    if TCP in pkt:
+      proto = 'tcp'
       sPort = pkt[TCP].sport
       dPort = pkt[TCP].dport
+   elif UDP in pkt:
+      proto = 'tcp'
+      sPort = pkt[UDP].sport
+      dPort = pkt[UDP].dport
 
-      print 'IP src = ' + str(ipSrc)
-      print 'IP dst = ' + str(ipDst)
-      print 'Source port = ' + str(sPort)
-      print 'Destination Port = ' + str(dPort)
-      print ' '
+   info['proto'] = proto
+   info['srcip'] = ipSrc
+   info['srcport'] = sPort
+   info['dstip'] = ipDst
+   info['dstport'] = dPort
+
+   return info
+      #print 'IP src = ' + str(ipSrc)
+      #print 'IP dst = ' + str(ipDst)
+      #print 'Source port = ' + str(sPort)
+      #print 'Destination Port = ' + str(dPort)
+      #print ' '
 
 def processData(pkt):
-   ruleText = "Secret"
-   hexText = ruleText.encode("HEX")
    hexData = str(pkt).encode("HEX")
 
    regex = re.compile(hexText)
    ret = regex.findall(hexData)
-
-   if len(ret) > 1:
-      print 'WARNING'
-      exit(0)
